199543f56f1985799cda2c8740fcd791df1d8179
Santiago <santiagodominguezarj@gmail.com>
2025-04-23 21:14:43 -0300

    Primera Version

---
+++ tp1_2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+int cuadrado(int num);
+void cuadradoVoid (int num);
+void direcMemo(int var);
+void cambiazo(int num1, int num2);
+void mayorMenor(int num1,int num2);
